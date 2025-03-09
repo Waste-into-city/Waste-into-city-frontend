5ce0ab7bbe7a4955a9bb050f3954377bd3138e3e
Aliaksandr Strukau <aliaksandr.strukau@modsen-software.com>
2025-03-09 22:16:16 +0300

    fix: moved server uri to .env

--- src/api/registerUser.ts
+++ src/api/registerUser.ts
@@ -1,24 +1,23 @@
 type UserLogs = Record<'name' | 'email' | 'password', string>;
 
+const API_URI = import.meta.env.VITE_BACKEND_URI;
+
 export const registerUser = async ({
 	email,
 	name,
 	password,
 }: UserLogs): Promise<void> => {
-	const response = await fetch(
-		'http://26.98.10.68:5111/api/v1/identity/register',
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				email,
-				password,
-				nickname: name,
-			}),
-		}
-	);
+	const response = await fetch(`${API_URI}/api/v1/identity/register`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			email,
+			password,
+			nickname: name,
+		}),
+	});
 	if (response.ok) {
 		return;
 	}
