3dcbf21cb853c6ba965d2fb3f4ec3ab28b8e5e21
Aliaksandr Strukau <aliaksandr.strukau@modsen-software.com>
2025-05-25 20:30:13 +0300

    fix: adde router for unit tests

--- src/utils/renderWithProviders.tsx
+++ src/utils/renderWithProviders.tsx
@@ -1,9 +1,14 @@
 import { ReactNode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
 import ThemeProvider from '@/components/providers/ThemeProvider';
 
 export const renderWithProviders = (children: ReactNode) => {
-	render(<ThemeProvider>{children}</ThemeProvider>);
+	render(
+		<ThemeProvider>
+			<BrowserRouter>{children}</BrowserRouter>
+		</ThemeProvider>
+	);
 };
